2e6f3f4216fbef409d570c4bab6bb9a8806ab99e
Pratik Ramteke <148608558+PratikPhysics@users.noreply.github.com>
2024-09-26 14:40:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.image("weather_girl.gif")
 
 def get_weather_data(city):
      api_key = "65435937efabb5df8e149e97f0f08e7e" # Replace with your API key
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
 
     try:
         response = requests.get(url)
